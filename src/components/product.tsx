6ddb0ef0ca3c11aa03be476996a656a91fa620d1
Alex <alpsnhandy@gmail.com>
2022-12-17 00:03:45 +0200

    reestablish show details

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -37,11 +37,11 @@ export function Product ({product}: ProductProps) {
     {details && <div>
     <p>{product.description}</p>
 
-    <p className='px-2 py-2 mt-2' style={{background: '#E6E6E6', borderRadius: '6px'}}>Rate: <span style={{ fontWeight: 'bold' }} >{product.rating.rate}</span></p>
+    <p className='px-2 py-2 mt-2' style={{background: '#E6E6E6', borderRadius: '6px'}}>Rate: <span style={{ fontWeight: 'bold' }} >{product?.rating?.rate}</span></p>
 
     <div className='border mt-2 rounded p-2 text-xs' style={{background: '#F2F2F2'}}>
       <p>category: <span className='pl-2' style={{color: 'grey'}}>{product.category}</span></p>
-      <p>quantity in stock: <span className='pl-2' style={{color: 'grey'}}>{product.rating.count}</span></p>
+      <p>quantity in stock: <span className='pl-2' style={{color: 'grey'}}>{product?.rating?.count}</span></p>
     </div>
 
     </div>}
